28b2a1b184cedc9e176c018333b8cac4027877e9
ppe <pepe@ender4.dsic.upv.es>
2008-11-14 17:24:28 +0100

    Switched to a Set to enforce the uniqueness invariant
    darcs-hash:20081114162428-98b34-4c05f86ec3b7546b519466e50f842de724a1b2f9.gz

--- src/ArgumentFiltering.hs
+++ src/ArgumentFiltering.hs
@@ -5,53 +5,64 @@
 
 module ArgumentFiltering where
 
-import Control.Arrow (first)
+import Control.Arrow (first, second)
+import Control.Monad (liftM)
 import Data.List ((\\))
+import Data.Map (Map)
+import Data.Set (Set)
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 import qualified Data.Foldable as Foldable
 import Data.Monoid
 import Prelude hiding (lookup, map)
+import qualified Prelude
 
 import Types
 import Utils
 
-newtype AF = AF {fromAF:: Map.Map Identifier [Int]} deriving (Eq, Ord, Show)
+newtype AF = AF {fromAF:: Map Identifier (Set Int)} deriving (Eq, Ord, Show)
 
 countPositionsFiltered = sum . fmap length . snd . unzip . toList
 
+empty     :: AF
 singleton :: Identifier -> [Int] -> AF
-cut    :: Identifier -> [Int] -> AF -> AF
-cutAll :: [(Identifier, [Int])] -> AF -> AF
-lookup :: Monad m => Identifier -> AF -> m [Int]
-fromList :: [(Identifier,[Int])] -> AF
-singleton id ii = AF (Map.singleton id ii)
-cut id ii (AF map) = AF $ Map.insertWith (flip(\\)) id ii map
-cutAll xx af = foldr (uncurry cut) af xx
-lookup id (AF map)    = maybe (fail "not found") return (Map.lookup id map)
-fromList = AF . Map.fromListWith (++)
-toList (AF af) = Map.toList af
-null (AF af) = Map.null af
+cut       :: Identifier -> [Int] -> AF -> AF
+cutAll    :: [(Identifier, [Int])] -> AF -> AF
+lookup    :: Monad m => Identifier -> AF -> m [Int]
+fromList  :: [(Identifier,[Int])] -> AF
+
+
+-- | left biased union
+union :: AF -> AF -> AF
+
+empty            = AF mempty
+singleton id ii  = AF (Map.singleton id (Set.fromList ii))
+cut id ii (AF m) = AF $ Map.insertWith (flip Set.difference) id (Set.fromList ii) m
+cutAll xx af     = foldr (uncurry cut) af xx
+lookup id (AF m) = maybe (fail "not found") (return.Set.toList) (Map.lookup id m)
+fromList         = AF . Map.fromListWith Set.union . Prelude.map (second Set.fromList)
+toList (AF af)   = Map.toList (Map.map Set.toList af)
+null (AF af)     = Map.null af
+union (AF m1) (AF m2) = AF$ Map.union m1 m2
+
 
 map :: (Identifier -> [Int] -> [Int]) -> AF -> AF
-map f (AF af) = AF (Map.mapWithKey f af)
+map f (AF af) = AF$ Map.mapWithKey (\k ii -> Set.fromList (f k (Set.toList ii))) af
 
 invert :: SignatureC a Identifier => a -> AF -> AF
-invert rules (AF af) = AF (Map.mapWithKey (\f ii -> [0..getArity sig f - 1] \\ ii) af)
+invert rules (AF af) = AF (Map.mapWithKey (\f ii -> Set.fromDistinctAscList [0..getArity sig f - 1] `Set.difference` ii) af)
   where sig = getSignature rules -- :: Signature (IdFunctions :+*: IdDPs)
 
 class ApplyAF t where applyAF :: AF -> t -> t
 instance (Functor f, ApplyAF a) => ApplyAF (f a) where applyAF af = fmap (applyAF af)
 instance (TRSC f, T Identifier :<: f) => ApplyAF (Term f) where
-    applyAF (AF af) = foldTerm f
+    applyAF af = foldTerm f
      where   f t | Just (T (n::Identifier) tt) <- prj t
-                 , Just ii       <- Map.lookup n af = term n (select tt ii)
+                 , Just ii       <- lookup n af = term n (select tt ii)
                  | otherwise = inject t
 
 instance ApplyAF (TRS Identifier f) where
     applyAF af trs@TRS{} = tRS$ applyAF af (rules trs)
 
-instance Monoid AF where
-  mempty  = AF Map.empty
-  mappend (AF m1) (AF m2) = AF$ Map.unionWith mappend m1 m2
-
-initAF t | sig <- getSignature t = fromList [ (d, [0.. getArity sig d -1]) | d <- Foldable.toList(definedSymbols sig `mappend` constructorSymbols sig)]
+initAF t | sig <- getSignature t = fromList [ (d, [0.. getArity sig d -1])
+                                                 | d <- Foldable.toList(definedSymbols sig `mappend` constructorSymbols sig)]
--- src/NarrowingProblem.hs
+++ src/NarrowingProblem.hs
@@ -8,7 +8,7 @@ import Data.Foldable (Foldable)
 import Data.List (sortBy)
 import Data.Monoid
 import Text.XHtml (toHtml, Html)
-import Prelude as P
+import Prelude as P hiding (fail)
 
 import qualified ArgumentFiltering as AF
 import DPairs
@@ -20,8 +20,8 @@ mkNDPProblem trs = Problem Narrowing trs (tRS $ getNPairs trs)
 
 afProcessor :: (IsVar f, AnnotateWithPos f f) => Problem f -> ProblemProgress Html f
 afProcessor p@(Problem Narrowing trs dps) = if null orProblems
-                                                  then Fail (AFProc mempty) p (toHtml "Could not find a grounding AF")
-                                                  else Or   (AFProc mempty) p (sortByDefinedness orProblems)
+                                                  then fail (AFProc AF.empty) p (toHtml "Could not find a grounding AF")
+                                                  else Or   (AFProc AF.empty) p (sortByDefinedness orProblems)
     where afs = snub $ map (findGroundAF p) (rules dps)
           orProblems = [And (AFProc af) p [NotDone $ AF.applyAF af (Problem Rewriting trs dps)] | Just af <- afs]
           sortByDefinedness = sortBy (flip compare `on` (AF.countPositionsFiltered . (\(And (AFProc af) _ _)-> af)))
@@ -33,11 +33,11 @@ findGroundAF p@(Problem _ trs@TRS{} dps) (_:->r)
   | otherwise =
     ( return . assertNoExtraVars . assertGroundDP . invariantExtraVars trs . invariantExtraVars dps) $ (mkGround r)
   where mkGround t
-          | null pp = mempty
+          | null pp = AF.empty
           | otherwise
           = AF.cutAll [ (root, [last p]) | p <- pp, let Just root = rootSymbol (t ! init p)] (AF.initAF p)
          where pp = varsPositions t
-        invariantSignature af = AF.fromList [ (IdFunction f, pp) | (IdDP f, pp) <- AF.toList af] `mappend` af
+        invariantSignature af = AF.fromList [ (IdFunction f, pp) | (IdDP f, pp) <- AF.toList af] `AF.union` af -- I disabled this invariant since I don't think it is necessary. It is not in the theory (I cannot find it, at least)
         invariantExtraVars trs@TRS{} af
           | null extra = af
           | otherwise  = invariantExtraVars trs $
