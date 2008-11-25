4ae391f0b07e1be45275cc417e73188bc166e8eb
pepe <mnislaih@gmail.com>
2008-11-25 18:09:49 +0100

    Optional PDF logs of the termination proofs
    Ignore-this: 6ac9cc2b07d468ea792765bb588bd90a
    
    darcs-hash:20081125170949-b62a5-18062d9c244c039123609586182e345ed06af7e1.gz

--- src/MainWeb.hs
+++ src/MainWeb.hs
@@ -1,17 +1,25 @@
-
+import Control.Exception (bracket)
+import Data.Maybe (isJust)
 import Network.CGI
 import TRS.FetchRules
 import TRS.FetchRules.TRS
+import Text.Printf
 import Text.XHtml
+
+import System.Cmd
+import System.FilePath
+import System.IO
 
 import Problem
 import Types hiding ((!))
+import Utils
 import Solver
 
 main = runCGI (handleErrors cgiMain)
 
 cgiMain = do
   mb_rules  <- getInput "TRS"
+  mb_visual <- getInput "LOG"
   case mb_rules of
     Nothing -> outputError 100 "missing parameter" []
     Just rules -> do
@@ -19,8 +27,18 @@ cgiMain = do
      case ei_trs of
        Left parse_error -> output $ show parse_error
        Right trs -> do
-          res <- liftIO $ solveNarrowingWeb $ mkTRS trs
+          res <- liftIO $ runSolver srvSolver $ mkTRS trs
+          proof_log <- if isJust mb_visual then do
+                                liftIO$ withTempFile "/tmp" "narradar-log-" $ \fp h -> do
+                                let fn = takeBaseName fp ++ ".pdf"
+                                hPutStrLn h (pprDot res)
+                                hClose h
+                                system (printf "dot -Tpdf %s -o /home/narradar/public_html/logs/%s" fp fn)
+                                return ("See a visual log of the proof " +++
+                                          anchor ! [href ("logs/" ++ fn)] << "here")
+                          else return noHtml
+
           output$ renderHtml $
            if isSuccess res
-             then thediv ! [identifier "title"] << h3 << "Termination was proved succesfully" +++ res
-             else thediv ! [identifier "title"] << h3 << "Termination could not be proved"    +++ res
+             then thediv ! [identifier "title"] << h3 << "Termination was proved succesfully." +++ proof_log +++ res
+             else thediv ! [identifier "title"] << h3 << "Termination could not be proved."    +++ proof_log +++ res
