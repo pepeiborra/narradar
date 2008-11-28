41c30aa0d90a127eced08be14c6f22cd64884743
pepe <mnislaih@gmail.com>
2008-11-28 10:03:04 +0100

    There were a couple of problems in the Narrowing processor
    Ignore-this: f7afb686768885bc8655c7f13e64bb2a
    
     - it wasn't checking the condition that the rhs does not unify with the next lhs
     - it was 'eating' pairs when they would not narrow to anything
     - it wasn't adding replacing a pair for all its narrowings, but only for one of them at a time
    
      and probably a bunch of more bad things
    
    darcs-hash:20081128090304-b62a5-6eda2a9f1967cb664127753f62bc7cfc40503531.gz

--- src/GraphTransformation.hs
+++ src/GraphTransformation.hs
@@ -1,7 +1,8 @@
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE PatternGuards, FlexibleContexts #-}
 
 module GraphTransformation where
 
+import Data.Maybe (isNothing)
 import Data.Array.IArray hiding ( array )
 import Control.Comonad.Pointer
 import Control.Monad.Logic
@@ -16,12 +17,16 @@ import qualified TRS
 
 narrowingProcessor :: (Hole :<: f) => Problem f -> ProblemProgress Html f
 narrowingProcessor p@(Problem Narrowing trs (TRS dps sig))
-  | null dpss = DontKnow{problem=p, procInfo=NarrowingP}
-  | otherwise = Or NarrowingP p [NotDone $ Problem Narrowing trs (TRS newdps sig) | newdps <- dpss]
-    where dpss = maps narrow1DP (filter (isLinear.rhs) dps)
+  | null dpss || [[]] == dpss = dontKnow NarrowingP p
+  | otherwise = orP NarrowingP p [return $ Problem Narrowing trs (TRS newdps sig) | newdps <- dpss]
+    where dpss = fst <$$> map concat (filter (all (not.null)) (maps (uncurry f) (zip dps (tail dps ++ dps))))
+          f dp@(_ :-> r) nxt@(l :-> _)
+              | isLinear r, isNothing (r `unify` l) = [(dp', nxt) | dp' <- narrow1DP dp]
+              | otherwise                           = []
           narrow1DP (l :-> r) = [(l TRS.// theta) :-> markDP r'| (r',theta) <- observeAll (narrow1 (rules trs) (unmarkDP r))]
+          (<$$>) = map . map
 
 -- I should teach myself about comonads
 -- http://sigfpe.blogspot.com/2008/03/comonadic-arrays.html
 maps f xx = concat $ elems $ array (Pointer 1 (listArray (1, length xx) xx) =>> appF) where
-    appF  = \(Pointer i a) ->  map elems [a // [(i, x)] | x <- f (a!i)]
+    appF  = \(Pointer i a) -> let a' = amap (:[]) a in map elems [a' // [(i, f(a!i))] ]
