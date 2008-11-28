5cd35aac19cf79ed8eb69c6c016c1294bae945d5
pepe <mnislaih@gmail.com>
2008-11-28 10:28:10 +0100

    Fix a bunch of problems in the Graph processor
    Ignore-this: d3cc62199acc680a254cca526569d1ab
    
     - The extra vars invariant was failing.
       We need to take the fixpoint of all the invariants together, since each invariant in the
       processing chain can modify the AF and thus disable a previously checked invariant
    
     - The processor was failing with an error on finding rules with a variable in the rhs
    
    darcs-hash:20081128092810-b62a5-16b1d6e1f8a83f9902a49cae40756dbfccda8bca.gz

--- src/NarrowingProblem.hs
+++ src/NarrowingProblem.hs
@@ -3,7 +3,10 @@
 
 module NarrowingProblem where
 
+import Control.Applicative
 import Control.Exception
+import Control.Monad
+import Control.Monad.Free
 import Data.Foldable (Foldable)
 import Data.List (sortBy)
 import Data.Monoid
@@ -16,14 +19,17 @@ import Problem
 import Utils
 import Types
 
+import TRS.Utils (fixM_Eq)
+
 mkNDPProblem trs = Problem Narrowing trs (tRS $ getNPairs trs)
 
 afProcessor :: (IsVar f, AnnotateWithPos f f) => Problem f -> ProblemProgress Html f
-afProcessor p@(Problem Narrowing trs dps) = if null orProblems
-                                                  then failP (AFProc AF.empty) p (toHtml "Could not find a grounding AF")
-                                                  else Or   (AFProc AF.empty) p (sortByDefinedness orProblems)
+afProcessor p@(Problem Narrowing trs dps) =
+    if null orProblems
+      then failP (AFProc AF.empty) p (toHtml "Could not find a grounding AF")
+      else orP   (AFProc AF.empty) p (Impure <$> sortByDefinedness orProblems)
     where afs = snub $ map (findGroundAF p) (rules dps)
-          orProblems = [And (AFProc af) p [NotDone $ AF.applyAF af (Problem Rewriting trs dps)] | Just af <- afs]
+          orProblems = [And (AFProc af) p [return $ AF.applyAF af (Problem Rewriting trs dps)] | Just af <- afs]
           sortByDefinedness = sortBy (flip compare `on` (AF.countPositionsFiltered . (\(And (AFProc af) _ _)-> af)))
 
 
@@ -31,23 +37,27 @@ findGroundAF :: (IsVar f, AnnotateWithPos f f) => Problem f -> DP f -> Maybe AF.
 findGroundAF p@(Problem _ trs@TRS{} dps) (_:->r)
   | isVar r   = Nothing
   | otherwise =
-    ( return . assertNoExtraVars . assertGroundDP . invariantExtraVars trs . invariantExtraVars dps) $ (mkGround r)
+      assertNoExtraVars . assertGroundDP <$> mkGround r >>= fixM_Eq (invariantExtraVars trs >=> invariantExtraVars dps)
   where mkGround t
-          | null pp = AF.empty
+          | isVar t = fail "mkGround: cannot make a variable ground"
+          | null pp || any null pp = return AF.empty
           | otherwise
-          = AF.cutAll [ (root, [last p]) | p <- pp, let Just root = rootSymbol (t ! init p)] (AF.initAF p)
+          = return $ AF.cutAll [ (root, [last p]) | p <- pp, let Just root = rootSymbol (t ! init p)] (AF.initAF p)
          where pp = varsPositions t
         invariantSignature af = AF.fromList [ (IdFunction f, pp) | (IdDP f, pp) <- AF.toList af] `AF.union` af -- I disabled this invariant since I don't think it is necessary. It is not in the theory (I cannot find it, at least)
         invariantExtraVars trs@TRS{} af
-          | null extra = af
-          | otherwise  = invariantExtraVars trs $
-                           AF.cutAll [ (root,[last p]) | rule@(_:->r) <- rules $ AF.applyAF af trs
-                                                       , let pp = varsPositions r
-                                                       , v <- extraVars rule
-                                                       , p <- pp
+          | null extra = return af
+          | otherwise  = invariantExtraVars trs =<<
+                           ((`AF.cutAll` af) . concat) <$>(
+                             forM (rules $ AF.applyAF af trs) $ \ rule@(_:->r) -> do
+                                           let extra = extraVars rule
+                                           guard (not (isVar r && not(null extra)))
+                                           return [ (root,[last p])
+                                                       | v <- extra
+                                                       , p <- varsPositions r
                                                        , r ! p == inject v
-                                                       , let Just root = rootSymbol (r ! init p)]
-                                  af
+                                                       , let Just root = rootSymbol (r ! init p)])
+
            where extra  = extraVars (AF.applyAF af trs)
         assertNoExtraVars af = assert (null $ extraVars $ AF.applyAF af trs) af
         assertGroundDP    af = assert (isGround $ AF.applyAF af r) af
