ff2243e27105ba211521eb3306d88db6e15f2ad9
pepe <mnislaih@gmail.com>
2008-11-28 10:55:52 +0100

    Update solvers to the free monad
    Ignore-this: b03f0aeab631588057c065b4d380c6aa
    
    darcs-hash:20081128095552-b62a5-d511acdd5c4dfb22c0e5eba952a2b7b24121457a.gz

--- src/Solver.hs
+++ src/Solver.hs
@@ -6,6 +6,7 @@
 module Solver where
 
 import Control.Applicative
+import Control.Monad.Free
 import "monad-param" Control.Monad.Parameterized
 import "monad-param" Control.Monad.MonadPlus.Parameterized
 import Text.XHtml (Html)
@@ -16,7 +17,6 @@ import GraphTransformation
 import Aprove
 import DPairs
 import Types
-import Operad
 import TRS.Utils
 
 import Prelude hiding (Monad(..))
@@ -30,33 +30,42 @@ webSolver, localSolver, srvSolver :: Solver Html IO f
 mainSolver :: Solver Html IO f -> Solver Html IO f
 
 -- solver that connects to the Aprove web site, use only for testing
-webSolver      = mainSolver (ProgressT . aproveWebProc)
+webSolver      = mainSolver (wrap' . aproveWebProc)
 
 -- solver that uses a cmd line version of Aprove, use only for testing
 localSolver       = localSolver' "/usr/local/lib/aprove/runme"
-localSolver' path = mainSolver (ProgressT . aproveProc path)
+localSolver' path = mainSolver (wrap' . aproveProc path)
 
 -- solver that uses an Aprove server running on the host machine
-srvSolver = mainSolver (ProgressT . aproveSrvProc)
+srvSolver = mainSolver (wrap' . aproveSrvProc)
+srvSolverSerial = mainSolverSerial (wrap' . aproveSrvProc)
 
 -- Attempts to connect to the Aprove local server, falls back to the Aprove web server
 --srvWebSolver = 
 
 -- Our main solving scheme
-mainSolver aproveProc = (cycleProcessor >=> afProcessor >||> aproveProc) `refineBy` narrowingProcessor
+mainSolverBase k = cycleProcessor >=>
+                   ((k afProcessor) `refineBy` (narrowingProcessor >=> cycleProcessor))
+
+
+mainSolver aproveProc = mainSolverBase (>||> aproveProc)
+mainSolverSerial aproveProc = mainSolverBase (>=> aproveProc)
 
 -- For debugging purposes
-mainSolverPure = (cycleProcessor >=> afProcessor) `refineBy` narrowingProcessor
+mainSolverPure = mainSolverBase id
 
+
+maxDepth = 2
 refineBy :: (Prelude.Monad m, Bind m' m m, MPlus m m m) => (a -> m a) -> (a -> m' a) -> a -> m a
-refineBy step refiner x = iterateMn 4 (refiner >=> step) (x >>= step)
+refineBy f refiner = loop maxDepth where
+  loop 0 x = f x
+  loop i x = f x `mplus` (refiner x >>= loop (i-1))
 
 runSolver :: (IsVar f, Hole :<: f, AnnotateWithPos f f, Types.Ppr f, Monad m) => Solver Html m f -> TRS Identifier f -> m (ProblemProgress Html f)
 runSolver solver trs@TRS{} = runProgressT (startSolver trs >>= solver)
 
 startSolver :: TRS Identifier f -> ProblemProgress Html f
 startSolver trs@TRS{} = returnM (mkNDPProblem trs)
-liftP proc = liftProgressT . proc
 
 (.|.) :: MPlus m m m => (b -> m a) -> (b -> m a) -> b -> m a
 f .|. g = \x -> f x `mplus` g x
