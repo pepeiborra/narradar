c416317770ae252bbed8ae19408eb71732e3cbfb
pepe <mnislaih@gmail.com>
2009-02-25 14:12:51 +0100

    Add a missing file
    Ignore-this: 895f66c83487b61d36ec18685d2bc520
    
    darcs-hash:20090225131251-b62a5-214d544067c8b272ad26643f4dec971172abc999.gz

---
+++ src/Bottom.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleInstances, FlexibleContexts #-}
+
+module Bottom where
+
+import Control.Applicative
+import Data.Foldable (Foldable(..))
+import Data.HashTable (hashString)
+import Data.Monoid
+import Data.Traversable
+import TRS
+import Text.PrettyPrint (text)
+
+
+data Bottom a = Bottom deriving (Show, Eq, Ord)
+instance Functor Bottom  where fmap f Bottom = Bottom
+instance Foldable Bottom where foldMap f Bottom = mempty
+instance Traversable Bottom where traverse f Bottom = pure Bottom
+instance Ppr Bottom      where pprF Bottom  = text "_|_"
+instance Zip Bottom      where fzipWith f _ = fail "fzipWith: Bottoms don't zip"
+instance HashTerm Bottom where hashF Bottom = hashString "Bottom"
+
+bottom :: (Bottom :<: f) => Term f
+bottom = inject Bottom
---
+++ src/UsableRules.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE PatternGuards, ViewPatterns #-}
+
+module UsableRules where
+
+import Control.Applicative
+import qualified Data.Foldable as F
+import Data.Maybe
+import Data.Monoid
+import qualified Data.Set as Set
+import Text.XHtml (Html)
+
+import DPairs
+import Identifiers
+import qualified ArgumentFiltering as AF
+import Proof
+import TRS
+import Types
+
+usableProcessor, iUsableProcessor :: (DPMark f id, Show id) => ProblemG id f -> ProblemProofG id Html f
+usableProcessor p@(Problem typ trs dps@TRS{}) | isBNarrowing typ
+   = step UsableRulesP p (mkProblem typ trs' dps)
+ where
+  dps' = maybe (rules dps) (`AF.apply` rules dps) (getGoalAF typ)
+  trs' = mkTRS (concat (usableRules mempty <$> rhs <$> dps'))
+  usableRules seen t = let ff  = Set.fromList [f | u <- subterms t, Just f <- [rootSymbol u]]
+                                 `Set.intersection` getDefinedSymbols trs
+                           new = Set.difference ff seen
+                           rr  = [r | r <- rules trs, fromJust(rootSymbol(lhs r)) `Set.member` new]
+                       in rr ++ concat (usableRules (ff `mappend` seen) <$> rhs <$> rr)
+
+usableProcessor p = return p
+
+-- FIX Is this a sound approximation of the Usable Rules ?
+--     I don't think so, as here we use pi to filter the rules too, but
+--     later we will use a more flexible version that filters less in the rules,
+--     but the same in the pairs.
+--     Fix it by using a version of pi restricted to DP symbols here.
+iUsableProcessor p@(Problem typ@(getGoalAF -> Just pi) trs dps@TRS{}) | isBNarrowing typ
+   = step UsableRulesP p (mkProblem typ trs' dps)
+ where
+  pi'  = AF.filter ((isDPSymbol.) . const) pi
+  trs' = mkTRS (F.toList $ usableRules mempty (rhs <$> pi_dps))
+  pi_dps = AF.apply pi (rules dps)
+  pi_trs = mkTRS(AF.apply pi (rules trs))
+  usableRules acc [] = acc
+  usableRules acc (t@(In in_t):rest)
+      | isVar t
+      = usableRules acc rest
+      | rr   <- Set.fromList [r | r <- rules trs, let r' = AF.apply pi r, In (icap pi_trs <$> in_t) `unifies` lhs r' ]
+      , new  <- Set.difference rr acc
+      = usableRules (rr `mappend` acc) (mconcat [rhs <$> F.toList new, directSubterms t, rest])
+
+iUsableProcessor p = return p
