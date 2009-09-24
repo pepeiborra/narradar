3fe939e675698d087768c44086bc95a3dd8c7bfc
Pepe Iborra <pepeiborra@gmail.com>
2009-09-24 14:58:02 +0200

    Revert "follow an API change in the graphviz package"
    This reverts commit f16abc8df450fc597f7cb3eb6bbf435f6414b803.
    
    Going back to the previous graphviz version

--- src/Narradar/Framework/GraphViz.hs
+++ src/Narradar/Framework/GraphViz.hs
@@ -79,7 +79,7 @@ instance (PprTPDBDot (NarradarProblem typ id), ProblemColor (NarradarProblem typ
     DotRep (NarradarProblem typ id) where
     dot p = Text (pprTPDBdot p) [ Color [problemColor p]
                                 , Shape BoxShape
-                                , Style [SItem Bold []]
+                                , Style (Stl Bold Nothing)
                                 , FontName "monospace"
                                 , FontSize 10
                                 , Margin (PVal (PointD 0.2 0.2))]
