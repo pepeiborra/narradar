6f1d437f1f179b8864d0986cb05337c8f8579a19
Pepe Iborra <pepeiborra@gmail.com>
2009-09-24 10:14:21 +0200

    Upload a sample strategy file

--- strategies/narr1Typ2.hs
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env runhaskell
-
-import Narradar
-import Narradar.Proof
-import Strats
-
-main = narradarMain $ \opts input -> do
-  (typ,pl)  <- parseProlog input
-  prologSolverOne opts (typeHeu2 typ) (typeHeu typ) pl
--- strategies/narrInn.hs
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env runhaskell
-
-import Narradar
-import Strats
-
-main = narradarMain $ \opts input -> do
-  (typ,pl)  <- parseProlog input
-  prologSolverAll opts (simpleHeu innermost) (typeHeu typ) pl
