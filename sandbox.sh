f81b773f940b4dea6e8d15fcf3369faac70953be
José Iborra <pepeiborra@gmail.com>
2014-09-20 18:30:51 +0100

    Added little cabal sandbox script

---
+++ sandbox.sh
@@ -0,0 +1,16 @@
+cabal sandbox init
+cabal sandbox add-source /Users/pepe/Dropbox/code/narradar-prolog
+cabal sandbox add-source /Users/pepe/Dropbox/code/narradar-prolog-tools
+cabal sandbox add-source /Users/pepe/Dropbox/code/narradar-alacarte
+cabal sandbox add-source /Users/pepe/Dropbox/code/narradar-term
+cabal sandbox add-source /Users/pepe/Dropbox/code/narradar-trsparser
+cabal sandbox add-source /Users/pepe/Dropbox/code/narradar-funsat-lang
+cabal sandbox add-source /Users/pepe/Dropbox/code/narradar-funsat-rpo
+cabal sandbox add-source /Users/pepe/Dropbox/code/forks/cgi-3001.1.8.5
+cabal sandbox add-source /Users/pepe/Dropbox/code/forks/yices-0.0.0.12
+cabal sandbox add-source /Users/pepe/Dropbox/code/forks/suitable-0.1.1
+cabal sandbox add-source /Users/pepe/Dropbox/code/forks/rmonad-0.8.0.2
+cabal sandbox add-source /Users/pepe/Dropbox/code/muterm-framework
+cabal sandbox add-source /Users/pepe/Dropbox/code/muterm-framework-charts
+cabal sandbox add-source /Users/pepe/Dropbox/code/forks/dotgen-0.3.1
+cabal sandbox add-source /Users/pepe/Dropbox/code/forks/Hoed-0.1.0.1
